490e46bc4b109eb0ddc247d52a3533a498f58010
Joel <jcbluemel@yahoo.com>
2022-06-29 12:22:46 -0700

    finished median and mode route functionality

--- app.js
+++ app.js
@@ -3,33 +3,58 @@
 const express = require("express");
 const app = express();
 
-const {findMean} = require("./stats");
+const { findMean, findMedian, findMode } = require("./stats");
+const { convertStrNums } = require("./utils");
 
 // useful error class to throw
-const { NotFoundError } = require("./expressError");
+const { NotFoundError, BadRequestError } = require("./expressError");
 const internal = require("stream");
 
 const MISSING = "Expected key `nums` with comma-separated list of numbers.";
 
 
 /** Finds mean of nums in qs: returns {operation: "mean", result } */
-app.get("/mean", function(req, res) {
-  let nums = req.query.nums.split(",");
-  let newNums = [];
-  for (let num of nums) {
-    newNums.push(Number(num));
+app.get("/mean", function (req, res) {
+
+  if (!(req.query.nums)) {
+    throw new BadRequestError(MISSING);
   }
-  console.log(newNums);
+
+  const nums = req.query.nums.split(",");
+  const newNums = convertStrNums(nums);
   const mean = findMean(newNums);
-  return res.json({response: { operation: "mean", value: mean,}});
-})
+
+  return res.json({ response: { operation: "mean", value: mean, } });
+});
 
 /** Finds median of nums in qs: returns {operation: "median", result } */
+app.get("/median", function (req, res) {
 
+  if (!(req.query.nums)) {
+    throw new BadRequestError(MISSING);
+  }
+
+  const nums = req.query.nums.split(",");
+  const newNums = convertStrNums(nums);
+  const mean = findMedian(newNums);
+
+  return res.json({ response: { operation: "median", value: mean, } });
+});
 
 
 /** Finds mode of nums in qs: returns {operation: "mean", result } */
+app.get("/mode", function (req, res) {
 
+  if (!(req.query.nums)) {
+    throw new BadRequestError(MISSING);
+  }
+
+  const nums = req.query.nums.split(",");
+  const newNums = convertStrNums(nums);
+  const mean = findMode(newNums);
+
+  return res.json({ response: { operation: "mode", value: mean, } });
+});
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
 app.use(function (req, res, next) {
--- utils.js
+++ utils.js
@@ -1,11 +1,19 @@
 const { BadRequestError } = require("./expressError");
 
 
-/** Convert strNums like ["1","2","3"] to [1, 2, 3]. */
+/** Convert strNums like ["1","2","3"] to [1, 2, 3].
+ *  if the conversion isn't successful, throw a BadRequestError.
+*/
 
 function convertStrNums(strNums) {
-  // if the conversion isn't successful, throw a BadRequestError and will
-  // be handled in your route
+
+  for (let num of strNums) {
+    if (isNaN(num)) {
+      throw new BadRequestError(`${num} is not a number.`);
+    }
+  }
+
+  return strNums.map(n => Number(n));
 }
 
 
